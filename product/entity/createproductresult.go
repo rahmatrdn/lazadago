08b4fbf8d4903585817351c0fe747bef21b943b8
Alfarady <radenalfa9@gmail.com>
2022-11-24 15:40:17 +0700

    fix: response entity create product detail

--- product/entity/createproductresult.go
+++ product/entity/createproductresult.go
@@ -1,19 +1,24 @@
 package entity
 
 import (
-    "github.com/wjpxxx/letgo/lib"
+	"github.com/wjpxxx/letgo/lib"
 )
 
-type CreateProductResult struct{
-    Data	CreateProductDataResponseEntity	`json:"data"`
-    Type	string	`json:"type"`
-    Code	string	`json:"code"`
-    Message	string	`json:"message"`
-    RequestId	string	`json:"request_id"`
-    Detail	[]CreateProductDetailResponseEntity	`json:"detail"`
-}
-func (g CreateProductResult) String() string {
-    return lib.ObjectToString(g)
+type CreateProductResult struct {
+	Data      CreateProductDataResponseEntity     `json:"data"`
+	Type      string                              `json:"type"`
+	Code      string                              `json:"code"`
+	Message   string                              `json:"message"`
+	RequestId string                              `json:"request_id"`
+	Detail    []CreateProductDetailResponseEntity `json:"detail"`
 }
 
-type CreateProductDetailResponseEntity struct{}
+func (g CreateProductResult) String() string {
+	return lib.ObjectToString(g)
+}
+
+type CreateProductDetailResponseEntity struct {
+	Field   string `json:"field"`
+	Message string `json:"message"`
+	Code    string `json:"code"`
+}
